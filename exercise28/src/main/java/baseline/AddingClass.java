590668ac08b0dd852a145d3d5c9f9d6bca5ccf0e
Blake Lochmandy <blakeloch@knights.ucf.edu>
2021-09-25 19:24:50 -0400

    Add exercise28 pseudocode skeleton

---
+++ exercise28/src/main/java/baseline/AddingClass.java
@@ -0,0 +1,30 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Blake Lochmandy
+ */
+
+package baseline;
+
+
+public class AddingClass {
+
+  // Create array of 5 numbers
+  private int[] numbers = new int[5];
+
+  public void getNumbersFromUser() {
+   // For each element of 'numbers'
+    // Prompt user to enter a number
+    // Store the number in 'numbers'
+  }
+
+  public int sumNumbers() {
+    // Initialize sum
+    // For each element of 'numbers'
+    // Add value to sum
+    // Return sum
+  }
+
+  public void printSum(int sum) {
+    // print sum
+  }
+}
---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Blake Lochmandy
+ */
+
+package baseline;
+
+public class Solution28 {
+
+  public static void main(String[] args) {
+
+    // Import AddingClass
+    AddingClass add = new AddingClass();
+
+    // Get user input
+    add.getNumbersFromUser();
+
+    // Add numbers together
+    int sum = add.sumNumbers();
+
+    // Print result
+    add.printSum(sum);
+
+
+
+  }
+
+}
