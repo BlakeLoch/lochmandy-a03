8f35470e4168591e5836922c5a6c87225de3b96c
Blake Lochmandy <blakeloch@knights.ucf.edu>
2021-09-25 23:52:59 -0400

    Add exercise30 pseudocode skeleton

---
+++ exercise30/src/main/java/baseline/MultiplicationClass.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Blake Lochmandy
+ */
+
+package baseline;
+
+public class MultiplicationClass {
+
+  public void printTimesTable() {
+
+    // Loop from 1 to 12
+      // Loop from 1 to 12
+        // multiply index of loop 1 by index of loop 2
+          // if number is less than 10
+            // print "     'product'"
+          // if number is greater than 99
+            // print "   'product'"
+          // if number is between 10 and 99
+            // print "    'product'"
+
+  }
+
+}
---
+++ exercise30/src/main/java/baseline/solution30.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Blake Lochmandy
+ */
+
+package baseline;
+
+public class solution30 {
+
+  public static void main(String[] args) {
+    // Link Multiplication Class
+    MultiplicationClass times = new MultiplicationClass();
+
+    // Generate Times Table
+    times.printTimesTable();
+  }
+
+}
